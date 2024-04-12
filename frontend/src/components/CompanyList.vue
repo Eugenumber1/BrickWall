394e7f52096bb82835c78d00c4963cbf36d1d321
ilyas issatayev <issatayev.ilyas@gmail.com>
2024-04-12 17:23:20 +0200

    Init basics

--- frontend/src/components/CompanyList.vue
+++ frontend/src/components/CompanyList.vue
@@ -1,11 +1,17 @@
 <template>
-    <div>
-        company list
+    <div class="flex w-full h-full">
+        <div class=" w-56">
+            menu placeholder
+        </div>
+        <CompanyPreview />
     </div>
 </template>
 
 <script setup>
+import CompanyPreview from './CompanyPreview.vue';
 import _ from 'lodash'
+
+
 
 console.log(_.random(5));
 </script>
--- frontend/src/components/CompanyPreview.vue
+++ frontend/src/components/CompanyPreview.vue
@@ -1,12 +1,32 @@
 <template>
-    <div></div>
+    <div class="flex w-full border rounded-md p-4" @click="onPreviewClick">
+        <div class="flex justify-between">
+            <div class="flex gap-x-4">
+                <img class=" w-16 h-16 bg-black rounded-md" src="./../assets/company_logo.webp" alt="">
+                <div class="flex flex-col">
+                    <h2 class=" text-xl">{{ name }}</h2>
+                    <div class="flex gap-x-4"> <span class=" text-sm">4.0</span> <i /></div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
 </template>
 
 <script setup>
+import { defineProps } from 'vue';
 const props = defineProps({
     name: {
         type: String,
         default: 'Company name'
     },
 })
+
+const onPreviewClick = () => {
+    let a = props;
+    console.log(a);
+}
 </script>
