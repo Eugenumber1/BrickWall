96eacda3a321beb3fef60c1b913b27e536c5fd17
ilyas issatayev <issatayev.ilyas@gmail.com>
2024-04-12 16:30:36 +0200

    Init Vue and co

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex w-full h-full min-h-screen">
+    <CompanyList></CompanyList>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import CompanyList from './components/CompanyList.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ frontend/src/components/CompanyList.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        company list
+    </div>
+</template>
+
+<script setup>
+</script>
