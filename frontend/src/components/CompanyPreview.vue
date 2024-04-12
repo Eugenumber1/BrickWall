23ed526ade57ca49030594bcb921b9d7d869bfd9
ilyas issatayev <issatayev.ilyas@gmail.com>
2024-04-12 17:40:14 +0200

    Prepare the base for the company preview

--- frontend/src/components/CompanyPreview.vue
+++ frontend/src/components/CompanyPreview.vue
@@ -1,17 +1,49 @@
 <template>
-    <div class="flex w-full border rounded-md p-4" @click="onPreviewClick">
-        <div class="flex justify-between">
+    <div class="flex justify-between w-full gap-y-3 border rounded-md p-4" @click="onPreviewClick">
+        <div class="flex flex-col gap-y-3">
             <div class="flex gap-x-4">
                 <img class=" w-16 h-16 bg-black rounded-md" src="./../assets/company_logo.webp" alt="">
                 <div class="flex flex-col">
                     <h2 class=" text-xl">{{ name }}</h2>
-                    <div class="flex gap-x-4"> <span class=" text-sm">4.0</span> <i /></div>
-
+                    <div class="flex gap-x-4"> <span class=" text-sm">4.0</span></div>
                 </div>
+            </div>
+            <div class="flex flex-col">
+                <span>Location:</span>
+                <span class="">{{ location }}</span>
+            </div>
+        </div>
+        <div class="flex mt-auto">
 
+            <div class="flex flex-col">
+                <span>Size:</span>
+                <span class="">{{ size }}</span>
             </div>
 
         </div>
+
+        <div class="flex flex-col gap-y-3">
+            <div class="flex gap-x-4">
+                <div class="flex flex-col gap-y-2">
+                    <span>{{ stats.reviewsCount }}K</span>
+                    <a>Reviews</a>
+                </div>
+                <div class="flex flex-col gap-y-2">
+                    <span>{{ stats.salariesCount }}K</span>
+                    <a>Salaries</a>
+                </div>
+                <div class="flex flex-col gap-y-2">
+                    <span>{{ stats.vacanciesCount }}K</span>
+                    <a>Vacancies</a>
+                </div>
+            </div>
+
+            <div class="flex flex-col">
+                <span>Industry:</span>
+                <span class="">{{ industry }}</span>
+            </div>
+        </div>
+
 
     </div>
 </template>
@@ -22,6 +54,25 @@ const props = defineProps({
     name: {
         type: String,
         default: 'Company name'
+    },
+    stats: {
+        default: {
+            reviewsCount: 0,
+            salariesCount: 0,
+            vacanciesCount: 0,
+        }
+    },
+    location: {
+        type: String,
+        default: 'Cryptostan'
+    },
+    size: {
+        type: Number,
+        default: 1,
+    },
+    industry: {
+        type: String,
+        default: 'crypto trading'
     },
 })
 
