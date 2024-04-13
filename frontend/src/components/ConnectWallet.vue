3aa23da3799723ba3edd970a4a5d5e826a8564e0
ilyas issatayev <issatayev.ilyas@gmail.com>
2024-04-13 14:29:46 +0200

    Update ConnectWallet

--- frontend/src/components/ConnectWallet.vue
+++ frontend/src/components/ConnectWallet.vue
@@ -2,11 +2,12 @@
     <button @click="connectWallet" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
         Connect Wallet <img :src="metamask" alt="MetaMask" class="h-6 mr-2">
     </button>
-
 </template>
 
 <script setup>
 import { ref } from "vue";
+import axios from 'axios'
+
 const metamask = "./metamask.png"
 const account = ref(null);
 
@@ -30,7 +31,18 @@ function handleAccountsChanged(accounts) {
     if (accounts.length === 0) {
         console.error('Please connect to MetaMask.');
     } else {
-        account.value = accounts[0];
+        let account_temp = accounts[0];
+        //account.value = accounts[0];
+        sendData(account_temp)
+    }
+}
+
+const sendData = async (account_temp) => {
+    try {
+        const response = await axios.post('http://localhost:3000/api/login/', account_temp);
+        account.value = response.data.walletHash;
+    } catch (error) {
+        console.log('Error sending data:', error)
     }
 }
 </script>
