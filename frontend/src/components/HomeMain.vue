86c428efa7b84b1b18159999303e7eeb20c424e8
ilyas issatayev <issatayev.ilyas@gmail.com>
2024-04-13 15:02:38 +0200

    Fix bug

--- frontend/src/components/HomeMain.vue
+++ frontend/src/components/HomeMain.vue
@@ -7,7 +7,7 @@
                 <span class="border-r border-b border-[#2b2b2b]  p-4">reviews</span>
                 <span class="border-r border-b border-[#2b2b2b]  p-4">avg. rating</span>
                 <span class="border-b border-[#2b2b2b]  p-4">clients</span>
-                <span class="border-r border-[#2b2b2b]  p-4">{{ companyCount }}</span>
+                <span class="border-r border-[#2b2b2b]  p-4">{{ companyCount ?? 44 }}</span>
                 <span class="border-r border-[#2b2b2b] p-4">{{ Math.floor(companyCount * 1.833) }}</span>
                 <span class="border-r border-[#2b2b2b]  p-4">4.4</span>
                 <span class=" p-4">8503</span>
@@ -23,5 +23,5 @@ import { useFiltersStore } from '@/filters.js';
 import { computed } from 'vue'
 
 const store = useFiltersStore();
-const companyCount = computed(() => store.companyList.value.length);
+const companyCount = computed(() => Array.isArray(store.companyList.value) ? store.companyList.value.length : 0);
 </script>
