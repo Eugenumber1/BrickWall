b21d89fb96eae104ca6f17adee9e393a46b677e4
ilyas issatayev <issatayev.ilyas@gmail.com>
2024-04-13 16:02:19 +0200

    Finish the preview

--- frontend/src/components/CompanyPreview.vue
+++ frontend/src/components/CompanyPreview.vue
@@ -1,9 +1,18 @@
 <template>
-    <div class="flex relative w-full rounded-md h-80" @click="onPreviewClick">
-        <img class="absolute h-full object-fill bg-black rounded-md blur-sm" src="./../assets/company_background.png"
-            alt="">
-        <div class="z-10 p-5 flex">
-            <span class="mt-auto text-lg font-semibold text-[#2b2b2b]">{{ name }}</span>
+    <div class="flex relative w-full rounded-md h-80 group overflow-hidden cursor-pointer" @click="onPreviewClick">
+        <img class="absolute h-full object-fill bg-black rounded-md blur-sm group-hover:scale-105 transition"
+            src="./../assets/company_background.png" alt="">
+        <div class="transition-all z-10 p-5 flex flex-col  group-hover:mb-8 ">
+            <span class="unbounded  mt-auto text-lg font-semibold text-[#2b2b2b]">{{ name }}</span>
+        </div>
+        <div class="absolute transition-all bottom-4 left-5 opacity-0 group-hover:opacity-100">
+            <div class="text-sm">crypto trading | Amsterdam | 50-200 buildors</div>
+        </div>
+        <div class="absolute transition-all opacity-0 group-hover:opacity-100 flex w-full h-full">
+            <div class="text-sm mb-auto mr-auto p-5 text-[#2b2b2b] font-semibold">4.4 / 10</div>
+        </div>
+        <div class="absolute transition-all opacity-0 group-hover:opacity-100 flex w-full h-full">
+            <button class="button--outline m-auto">add review</button>
         </div>
     </div>
 </template>
