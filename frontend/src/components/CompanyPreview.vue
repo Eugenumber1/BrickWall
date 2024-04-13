d40aad72f29ec51392463c6c31dc9cbb83395cd3
ilyas issatayev <issatayev.ilyas@gmail.com>
2024-04-13 14:54:04 +0200

    Style the preview

--- frontend/src/components/CompanyPreview.vue
+++ frontend/src/components/CompanyPreview.vue
@@ -1,50 +1,10 @@
 <template>
-    <div class="flex justify-between w-full gap-y-3 border-2 rounded-md p-4" @click="onPreviewClick">
-        <div class="flex flex-col gap-y-3">
-            <div class="flex gap-x-4">
-                <img class=" w-16 h-16 bg-black rounded-md" src="./../assets/company_logo.webp" alt="">
-                <div class="flex flex-col">
-                    <h2 class=" text-xl">{{ name }}</h2>
-                    <div class="flex gap-x-4"> <span class=" text-sm">4.0</span></div>
-                </div>
-            </div>
-            <div class="flex flex-col">
-                <span>Location:</span>
-                <span class="">{{ location }}</span>
-            </div>
+    <div class="flex relative w-full rounded-md h-80" @click="onPreviewClick">
+        <img class="absolute h-full object-fill bg-black rounded-md blur-sm" src="./../assets/company_background.png"
+            alt="">
+        <div class="z-10 p-5 flex">
+            <span class="mt-auto text-lg font-semibold text-[#2b2b2b]">{{ name }}</span>
         </div>
-        <div class="flex mt-auto">
-
-            <div class="flex flex-col">
-                <span>Size:</span>
-                <span class="">{{ size }}</span>
-            </div>
-
-        </div>
-
-        <div class="flex flex-col gap-y-3">
-            <div class="flex gap-x-4">
-                <div class="flex flex-col gap-y-2">
-                    <span>{{ stats.reviewsCount }}K</span>
-                    <a>Reviews</a>
-                </div>
-                <div class="flex flex-col gap-y-2">
-                    <span>{{ stats.salariesCount }}K</span>
-                    <a>Salaries</a>
-                </div>
-                <div class="flex flex-col gap-y-2">
-                    <span>{{ stats.vacanciesCount }}K</span>
-                    <a>Vacancies</a>
-                </div>
-            </div>
-
-            <div class="flex flex-col">
-                <span>Industry:</span>
-                <span class="">{{ industry }}</span>
-            </div>
-        </div>
-
-
     </div>
 </template>
 
