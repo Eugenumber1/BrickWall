ccbbe675da00c0e02956ad8231cca04c2d92bf9b
Ilya <ilya@sinyugin.ru>
2024-04-13 12:29:18 +0200

    api changes & bandada functions

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,17 @@ const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bandada = require('./bandada');
 const app = express();
-app.use(cors());
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const pr_key = process.env.PR_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
+app.use(express.json());
+
+app.use(cors());
 
 const port = 3000;
 
@@ -22,62 +25,55 @@ app.get('/api/status', (req, res) => {
     res.json({ status: 'Server is running', timestamp: new Date() });
 });
 
-app.get('/api/companylist', (req, res) => {
-    // TODO: retrieve the list of companies from db
-    const list = [
-        { id: 1, name: 'Company One' },
-        { id: 2, name: 'Company Two' },
-        { id: 3, name: 'Company Three' },
-        { id: 4, name: 'Company Four' },
-    ];
+app.get('/api/companylist', async (req, res) => {
+    const list = await bandada.getAllGroups();
     res.json(list);
 });
 
-app.get('/api/company/:id', (req, res) => {
-    // TODO: fetch company(group) data from db 
-    const id = parseInt(req.params.id);
-    const company = { id, name: `Company ${id}` };
+app.get('/api/company/:id', async (req, res) => {
+    const id = req.params.id;
+    const company = await bandada.getGroupById(id.toString());
     res.json(company);
 });
 
-app.get('/api/company/:id/proof', (req, res) => {
-    // TODO: retrieve merkle proof for a company(group)
-    // invoke a function from bandada.js
-    const id = parseInt(req.params.id);
-    const proof = { id, proof: '0x1234567890' };
+app.get('/api/company/:id/member/:memberid/proof', async (req, res) => {
+    const id = req.params.id;
+    const memberId = req.params.memberid;
+    const proof = await bandada.getGroupProofByUId(id.toString(), memberId.toString());
     res.json(proof);
 });
 
-app.get('/api/company/:id/members', (req, res) => {
-    // TODO: retrieve all members of a company(group)
-    // invoke a function from bandada.js
-    const id = parseInt(req.params.id);
-    const members = [
-        { id: 1, name: 'Alice' },
-        { id: 2, name: 'Bob' },
-        { id: 3, name: 'Charlie' },
-    ];
+app.get('/api/company/:id/members', async (req, res) => {
+    const id = req.params.id;
+    const groupData = await bandada.getGroupById(id.toString());
+    const members = groupData.group.members;
     res.json(members);
 });
 
+app.post('/api/company', async (req, res) => {
+    console.log(`Creating company with data: ${JSON.stringify(req.body)}`)
+    const name = req.body.name;
+    const description = req.body.description;
+    const created = await bandada.createGroup(name, description);
+    res.json(created);
+});
+
 app.post('/api/company/:id/review', (req, res) => {
-    // TODO: upload a review for a company(group)
-    // invoke a function from bandada.js 
+    // TODO: upload a review for a company(group) to DB
     const id = parseInt(req.params.id);
     const review = req.body;
     res.json({ id, review });
 });
 
-app.post('/api/company/:id/member', (req, res) => {
-    // TODO: add a member to a company(group)
-    // invoke a function from bandada.js
-    const id = parseInt(req.params.id);
-    const member = req.body;
-    res.json({ id, member });
+app.post('/api/company/:id/member/:memberid', async (req, res) => {
+    const id = req.params.id;
+    const memberId = req.params.memberid;
+    const added = await bandada.addMemberToGroup(id, memberId);
+    res.json({ status: added, member: memberId, group: id });
 });
 
 app.delete('/api/company/:id/review/:reviewId', (req, res) => {
-    // TODO: remove a review from a company(group)
+    // TODO: remove a review from a DB
     const id = parseInt(req.params.id);
     const reviewId = parseInt(req.params.reviewId);
     res.json({ id, reviewId });
@@ -88,6 +84,20 @@ app.delete('/api/company/:id/member/:memberId', (req, res) => {
     const id = parseInt(req.params.id);
     const memberId = parseInt(req.params.memberId);
     res.json({ id, memberId });
+});
+
+app.delete('/api/company/:id/member/:memberId', async (req, res) => {
+    const id = req.params.id;
+    const memberId = req.params.memberId;
+    // remove member from group 
+    const removed = await bandada.removeMemberFromGroup(id, memberId);
+    res.json({ status: removed, member: memberId, group: id });
+});
+
+app.delete('/api/company/:id', async (req, res) => {
+    const id = req.params.id;
+    const removed = await bandada.removeGroups([id]);
+    res.json({ status: removed, group: id });
 });
 
 /////////////////////////// DB ///////////////////////////
