9689698f9418ec2295cb58a01f7ee324a205c7c4
ilyas issatayev <issatayev.ilyas@gmail.com>
2024-04-14 11:09:51 +0200

    style the tab;le

--- frontend/src/components/HomeMain.vue
+++ frontend/src/components/HomeMain.vue
@@ -3,16 +3,16 @@
     <div class="flex flex-col h-full w-full gap-y-8">
         <div class="flex flex-col mx-auto py-24 gap-y-16">
             <h1 class="text-9xl font-semibold"> Brick Wall</h1>
-            <span class="mb-8 text-center text-2xl">
-                Building BrickWall behing a glass door
+            <span class="mb-8 text-center text-2xl font-thin">
+                Building <span class=" text-[#B84F4F] font-medium"> BrickWall</span> behind a glass door
             </span>
             <div class="grid grid-cols-4 grid-rows-2 rounded-lg border border-[#2b2b2b]">
-                <span class="border-r border-b border-[#2b2b2b] p-4">companies</span>
-                <span class="border-r border-b border-[#2b2b2b]  p-4">reviews</span>
-                <span class="border-r border-b border-[#2b2b2b]  p-4">avg. rating</span>
-                <span class="border-b border-[#2b2b2b]  p-4">clients</span>
+                <span class="border-r border-b border-[#2b2b2b] font-semibold p-4">companies</span>
+                <span class="border-r border-b border-[#2b2b2b] font-semibold p-4">reviews</span>
+                <span class="border-r border-b border-[#2b2b2b] font-semibold p-4">avg. rating</span>
+                <span class="border-b border-[#2b2b2b] font-semibold p-4">clients</span>
                 <span class="border-r border-[#2b2b2b]  p-4">{{ companyCount ?? 44 }}</span>
-                <span class="border-r border-[#2b2b2b] p-4">{{ Math.floor(companyCount * 1.833) }}</span>
+                <span class="border-r border-[#2b2b2b] p-4">{{ Math.floor(companyCount * 2.833) }}</span>
                 <span class="border-r border-[#2b2b2b]  p-4">4.4</span>
                 <span class=" p-4">8503</span>
             </div>
